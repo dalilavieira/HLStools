3287712c36f324a827d3815020eba088f323f98b
canesche <michael.canesche@gmail.com>
2020-04-06 11:28:34 -0300

    update

--- teste_3_plataformas/intel/bubblesort/a.prj/components/bubbleSort/bubbleSort/bubbleSort_internal_10/synth/bubbleSort_i_llvm_fpga_pipeline_keep_going35_bubblesort1_valid_fifo.sv
+++ teste_3_plataformas/intel/bubblesort/a.prj/components/bubbleSort/bubbleSort/bubbleSort_internal_10/synth/bubbleSort_i_llvm_fpga_pipeline_keep_going35_bubblesort1_valid_fifo.sv
@@ -15,7 +15,7 @@
 // ---------------------------------------------------------------------------
 
 // SystemVerilog created from bubbleSort_i_llvm_fpga_pipeline_keep_going35_bubblesort1_valid_fifo
-// SystemVerilog created on Mon Apr  6 10:26:57 2020
+// SystemVerilog created on Mon Apr  6 11:16:20 2020
 
 
 (* altera_attribute = "-name AUTO_SHIFT_REGISTER_RECOGNITION OFF; -name MESSAGE_DISABLE 10036; -name MESSAGE_DISABLE 10037; -name MESSAGE_DISABLE 14130; -name MESSAGE_DISABLE 14320; -name MESSAGE_DISABLE 15400; -name MESSAGE_DISABLE 14130; -name MESSAGE_DISABLE 10036; -name MESSAGE_DISABLE 12020; -name MESSAGE_DISABLE 12030; -name MESSAGE_DISABLE 12010; -name MESSAGE_DISABLE 12110; -name MESSAGE_DISABLE 14320; -name MESSAGE_DISABLE 13410; -name MESSAGE_DISABLE 113007; -name MESSAGE_DISABLE 10958" *)
--- teste_3_plataformas/intel/prod_matrix/a.prj/components/prod_matrix/ip/prod_matrix_i_llvm_fpga_push_i1_memdep_phi3_pop1542_push31_0.sv
+++ teste_3_plataformas/intel/prod_matrix/a.prj/components/prod_matrix/ip/prod_matrix_i_llvm_fpga_push_i1_memdep_phi3_pop1542_push31_0.sv
@@ -15,7 +15,7 @@
 // ---------------------------------------------------------------------------
 
 // SystemVerilog created from prod_matrix_i_llvm_fpga_push_i1_memdep_phi3_pop1542_push31_0
-// SystemVerilog created on Mon Apr  6 10:28:45 2020
+// SystemVerilog created on Mon Apr  6 11:18:08 2020
 
 
 (* altera_attribute = "-name AUTO_SHIFT_REGISTER_RECOGNITION OFF; -name MESSAGE_DISABLE 10036; -name MESSAGE_DISABLE 10037; -name MESSAGE_DISABLE 14130; -name MESSAGE_DISABLE 14320; -name MESSAGE_DISABLE 15400; -name MESSAGE_DISABLE 14130; -name MESSAGE_DISABLE 10036; -name MESSAGE_DISABLE 12020; -name MESSAGE_DISABLE 12030; -name MESSAGE_DISABLE 12010; -name MESSAGE_DISABLE 12110; -name MESSAGE_DISABLE 14320; -name MESSAGE_DISABLE 13410; -name MESSAGE_DISABLE 113007; -name MESSAGE_DISABLE 10958" *)
