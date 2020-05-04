39ba88c53954a7a52afb540e17fc32b9729b0263
JnCM <josuenunes014@gmail.com>
2020-05-04 13:49:13 -0300

    DE2-115 verilog codes

---
+++ LegUp/test_algorithms/Intel/DE2-115/Mmc/mmc.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int mmc(int a, int b) {
+    int F1 = a, F2 = b;
+
+    while (F1 != F2){
+        if (F1 > F2)
+            F2 += b;
+        else
+            F1 += a;
+    }
+    return F1;
+}
+
+int main(){
+    int a = 6, b = 7;
+
+    int r;
+    r = mmc(a, b);
+
+    printf("mmc of %d and %d is %d\n", a, b, r);
+
+    return 0;
+}
