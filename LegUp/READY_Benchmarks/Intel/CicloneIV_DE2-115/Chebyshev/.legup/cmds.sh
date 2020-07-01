755b21718914f1836f344625c8894c13f08d1255
Maria Dalila Vieira <mdadavivi@gmail.com>
2020-07-01 17:47:28 +0000

    organiza dir LegUp

--- LegUp/READY_Benchmarks/Intel/CicloneIV_DE2-115/Chebyshev/.legup/cmds.sh
+++
@@ -1,3 +0,0 @@
-make sw_compile
-LD_LIBRARY_PATH=/cygdrive/c/LegUp-7.5/legup/legup-library/legup:/cygdrive/c/LegUp-7.5/legup/legup-library:/cygdrive/c/LegUp-7.5/legup/legup-library/legup/:/cygdrive/c/LegUp-7.5/legup/dependencies/lib:/cygdrive/c/LegUp-7.5/legup/dependencies/lib/coinlp:/cygdrive/c/LegUp-7.5/legup/dependencies/gcc/lib64: ./Chebyshev.sw_binary || true  
-# Ignores the return value of main function.
