cc08dcd269d89d1ab039d9630f0554fc0702ada7
canesche <michael.canesche@gmail.com>
2020-08-16 15:34:44 -0300

    update

--- example/Vivado/example3/example3.c
+++ example/Vivado/example3/example3.c
@@ -1,23 +1,24 @@
-// Example 3
+// Example 3 // gcc -Wall -pedantic example6_test3.c 
 
 #include <stdlib.h>
 #include <stdio.h>
 
-int *test(void *a) {
-
-	int *b = a;
-
-	return b;
-}
+/*
+	int a0 = 17, a1 = 23, a2 = 31;
+	int *b0 = argc > 1 ? &a0 : &a1;
+	int *b1 = &a2;
+	int **c =  argc > 3 ? &b0 : &b1;	
+*/
 
 int main() {
 
-	int c = 0; 
+	int t;
+	
+	scanf("%d", &t);
 
-	int *b;
-	int *a = &c;
+	int a0 = 17, a1 = 13;
+	int *b = t > 1 ? &a0 : &a1;
+	int **c = &b;	
 
-	b = test(a);	
-
-	return (unsigned long) b;
+	return **c;
 }
